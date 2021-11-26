a8a03f2114c624e710e11a678f15fdb5774834b1
moad machkouri <moad.machkouri07@gmail.com>
2021-11-26 20:33:03 +0200

    cleaning vueapp and add navbar and header app

---
+++ src/components/AppNavTop.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav>
+      <h1>logo</h1>
+      <div class="search"></div>
+      <button>cart</button>
+      <button>account</button>
+  </nav>
+</template>
+
+<script>
+export default {
+  name:'NavbarTop',
+
+}
+</script>
+
+<style lang="scss">
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <navbar-top />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import NavbarTop from '@/components/AppNavTop.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    NavbarTop
   }
 }
 </script>
